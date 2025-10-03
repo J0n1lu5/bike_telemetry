dca7448b8130936b4b6bf3f7ab6f31671ca6c969
J0n1lu5 <j.pluder@mci4me.at>
2025-10-03 23:49:23 +0200

    commented out calculation + added data logging to sd card ... not working

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,16 @@
 #include <Wire.h>
 #include <MPU6050.h>
 #include <math.h>
+#include <SD.h>
+#include <SPI.h>
 
 MPU6050 mpu1(0x68);
 MPU6050 mpu2(0x69);
+
+#define SD_CS   5      // Chip-Select
+#define SD_SCK  18     // VSPI_SCK
+#define SD_MISO 19     // VSPI_MISO
+#define SD_MOSI 23     // VSPI_MOSI
 
 // factor for scaling to m/s^2
 const float ACC_SCALE = 9.81 / 16384.0;
@@ -17,7 +24,7 @@ unsigned int  dt = 0;
 // velocity
 float dist = 0;
 
-float calc_dist(float accx, float accy, float accz, unsigned int dt){
+float calc_dist(float accx, float accy, float accz, unsigned int dt, float baseline[]){
   float vx = 0;
   float vy = 0;
   float vz = 0;
@@ -32,9 +39,9 @@ float calc_dist(float accx, float accy, float accz, unsigned int dt){
 
   scale_dt = dt / 1e6;
 
-  vx = accx * scale_dt;
-  vy = accy * scale_dt;
-  vz = accz * scale_dt;
+  vx = (accx - baseline[0]) * scale_dt;
+  vy = (accy - baseline[1]) * scale_dt;
+  vz = (accz - baseline[2]) * scale_dt;
 
   sx = vx * scale_dt;
   sy = vy * scale_dt;
@@ -45,8 +52,12 @@ float calc_dist(float accx, float accy, float accz, unsigned int dt){
   return distance;
 }
 
+
 void setup() {
   Serial.begin(115200);
+  
+  
+  //MPU setup
   Wire.begin(21, 22); // ESP32: SDA=21, SCL=22
   
   mpu1.initialize();
@@ -66,6 +77,27 @@ void setup() {
   } else {
     Serial.println("Fehler beim Verbinden mit MPU6050.");
   }
+
+
+  //SD card setup
+  SPI.begin(SD_SCK, SD_MISO, SD_MOSI, SD_CS);
+
+  if (!SD.begin(SD_CS)) {
+    Serial.println("SD-Karte nicht gefunden!");
+    return;
+  }
+  Serial.println("SD-Karte initialisiert.");
+
+  File Data = SD.open("/Data.csv", FILE_WRITE); 
+  if (Data) {
+    Data.println("timestamp_ms,x1,y1,z1,x2,y2,z2"); 
+    Data.close();
+  }
+  
+
+  
+
+  pinMode(13, INPUT_PULLUP);
 }
 
 
@@ -73,6 +105,7 @@ void loop() {
   
   int16_t ax1, ay1, az1;
   int16_t ax2, ay2, az2;
+  float baseline[3] = {0};
 
   //get sensor data
   mpu1.getAcceleration(&ax1, &ay1, &az1);
@@ -80,6 +113,39 @@ void loop() {
 
   //get time
   t = micros();
+  
+//acceleration in m/s^2
+  float accx1 = ax1 * ACC_SCALE;
+  float accy1 = ay1 * ACC_SCALE;
+  float accz1 = az1 * ACC_SCALE;
+
+  float accx2 = ax2 * ACC_SCALE;
+  float accy2 = ay2 * ACC_SCALE;
+  float accz2 = az2 * ACC_SCALE;
+
+
+  String line;
+    line.reserve(128);
+    line += String(t);
+    line += ",";
+    line += String(accx1,5); 
+    line += ",";
+    line += String(accy1,5); 
+    line += ",";
+    line += String(accz1,5); 
+    line += ",";
+    line += String(accx2,5); 
+    line += ",";
+    line += String(accy2,5); 
+    line += ",";
+    line += String(accz2,5); 
+
+  File Data = SD.open("/Data", FILE_APPEND);
+  Data.println(line);
+  Data.close();
+  
+  
+  /*
   dt = t - last_time;
   last_time = t;
 
@@ -92,26 +158,36 @@ void loop() {
   float accy2 = ay2 * ACC_SCALE;
   float accz2 = az2 * ACC_SCALE;
 
-  //erdbeschleunigung muss rausgerechnet werden
 
-  //entweder über konstanten wert und oder durch init
 
   //relative accelaration
   float accx = accx1 - accx2;
   float accy = accy1 - accy2;
   float accz = accz1 - accz2;
   
-  /*
+  
   //print sensor data
   Serial.print("Beschleunigung X: "); Serial.print(accx1);
   Serial.print(" | Y: "); Serial.print(accy1);
   Serial.print(" | Z: "); Serial.println(accz1);
-  */
+  
+
+
+// eleminate baseline noise  
+ if (digitalRead(13) == LOW){
+  baseline[0] = accx;
+  baseline[1] = accy;
+  baseline[2] = accz;
+ }
 
   //calculating distance
-
-  dist = calc_dist(accx, accy, accz, dt);
+ 
+  dist = calc_dist(accx, accy, accz, dt, baseline);
   Serial.print("Distance:");
   Serial.print(dist,6);
   Serial.print("\n");
+
+  */
+
+  
 }
